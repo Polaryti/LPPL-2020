0e4b688b410ae846b80376155bd17ac073a9d745
Polaryti <mario.identation@gmail.com>
2020-12-09 10:45:00 +0100

    Update header.h

--- Parte II/include/header.h
+++ Parte II/include/header.h
@@ -9,6 +9,9 @@
 /****************************************************** Constantes generales */
 #define TRUE  1
 #define FALSE 0
+#define TALLA_TIPO_SIMPLE 1     /* Talla asociada a los tipos simples */
+#define TALLA_SEGENLACES 2      /* Talla del segmento de Enlaces de Control */
+
 
 /************************************* Variables externas definidas en el AL */
 extern int yylex();
@@ -17,12 +20,16 @@ extern int yyparse();
 extern FILE *yyin;                           /* Fichero de entrada           */
 extern int   yylineno;                       /* Contador del numero de linea */
 extern char *yytext;                         /* Patron detectado             */
+
 /********* Funciones y variables externas definidas en el Programa Principal */
-extern void yyerror(const char * msg) ;   /* Tratamiento de errores          */
+extern void yyerror(const char * msg) ;     /* Tratamiento de errores          */
 
-extern int verbosidad;                   /* Flag si se desea una traza       */
-extern int numErrores;              /* Contador del numero de errores        */
+extern int verbosidad;                      /* Flag si se desea una traza       */
+extern int numErrores;                      /* Contador del numero de errores        */
+extern int verTdS;                          /* Flag para saber si mostrar la TdS */
 
+extern int dvar;                            /* Desplazamiento en el Segmento de Variables */
+extern int niv;                             /* Nivel de anidamiento "global" o "local" */
 
 #endif  /* _HEADER_H */
 /*****************************************************************************/
