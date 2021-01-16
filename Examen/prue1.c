46dec73e71d020c14dcdbb7025e06004696850f7
Polaryti <mario.identation@gmail.com>
2021-01-16 10:06:16 +0100

    FINAL

---
+++ Examen/prue1.c
@@ -0,0 +1,13 @@
+// Ejemplo sencillo, con dos errores semanticos,
+// del uso de la instruccion "UsingTimes"
+int main ()
+{ bool a; bool x; int res;
+
+  read(x);
+  res = 0;
+  using a 				// Variable no es entera
+  x times 				// Expresion no es entera
+  do res = res+1 ; 
+
+  return 0;
+}
